08e881165794763f864edc4e30bde2ad38185cb4
quentin roux <quentin.roux@hotmail.fr>
2020-07-07 20:06:03 +0200

    fibonacci done

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,37 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+const memo = {};
+
+function fib(n) {
+  // ITERATIVE
+
+  // let arr = [];
+  // for (let i = 0; i <= n; i++) {
+  //   if (i === 0) arr.push(0);
+  //   if (i === 1) arr.push(1);
+  //   if (i > 1) arr.push(arr[i - 2] + arr[i - 1]);
+  // }
+
+  // return arr[arr.length - 1];
+
+  // RECURSIVE
+  // if (n < 2) return n;
+  // if (n >= 2) {
+  //   return fib(n - 2) + fib(n - 1);
+  // }
+
+  // MMOIZED version
+  if (n < 2) return n;
+  if (n >= 2) {
+    if (memo[n]) {
+      return memo[n];
+    } else if (memo[n - 2] && memo[n - 1]) {
+      return (memo[n] = memo[n - 2] + memo[n - 1]);
+    } else {
+      return (memo[n] = fib(n - 2) + fib(n - 1));
+    }
+  }
+}
 
 module.exports = fib;
