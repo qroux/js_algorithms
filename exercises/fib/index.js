f09fb617965d631c74adf84de08e993693bdf2fb
quentin roux <quentin.roux@hotmail.fr>
2020-07-22 18:59:44 +0200

    linked list done

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,30 @@
 // Example:
 //   fib(4) === 3
 
+// function memoize(fn) {
+//   const cache = {};
+
+//   return function (...args) {
+//     if (cache[args]) {
+//       return cache[args];
+//     }
+
+//     const result = fn.apply(this, args);
+//     cache[args] = result;
+
+//     return result;
+//   };
+// }
+
+// function slowFib(n) {
+//   if (n < 2) return n;
+//   if (n >= 2) {
+//     return fib(n - 2) + fib(n - 1);
+//   }
+// }
+
+// const fib = memoize(slowFib);
+
 function memoize(fn) {
   const cache = {};
 
@@ -26,7 +50,7 @@ function memoize(fn) {
 function slowFib(n) {
   if (n < 2) return n;
   if (n >= 2) {
-    return fib(n - 2) + fib(n - 1);
+    return fib(n - 1) + fib(n - 2);
   }
 }
 
--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -2,8 +2,99 @@
 // Implement classes Node and Linked Lists
 // See 'directions' document
 
-class Node {}
+class Node {
+  constructor(data, next = null) {
+    this.data = data;
+    this.next = next;
+  }
+}
 
-class LinkedList {}
+class LinkedList {
+  constructor() {
+    // const firstNode = new Node('first');
+
+    this.head = null;
+  }
+
+  insertFirst(n) {
+    this.head = new Node(n, this.head);
+  }
+
+  size() {
+    let counter = 0;
+    let node = this.head;
+
+    while (node) {
+      node = node.next;
+      counter++;
+    }
+
+    return counter;
+  }
+
+  getFirst() {
+    return this.head;
+  }
+
+  getLast() {
+    let node = this.head;
+
+    while (node) {
+      if (node.next) {
+        node = node.next;
+      } else {
+        return node;
+      }
+    }
+  }
+
+  clear() {
+    this.head = null;
+  }
+
+  removeFirst() {
+    if (!this.head) return;
+    this.head = this.head.next;
+  }
+
+  removeLast() {
+    if (!this.head) return;
+    if (this.head === this.getLast()) return (this.head = null);
+
+    let previous = this.head;
+    let node = previous.next;
+
+    while (node.next) {
+      previous = node;
+      node = node.next;
+    }
+
+    previous.next = null;
+  }
+
+  insertLast(data) {
+    const last = this.getLast();
+    if (last) {
+      last.next = new Node(data);
+    } else {
+      this.head = new Node(data);
+    }
+  }
+
+  getAt(index) {
+    let counter = 0;
+    let node = this.head;
+
+    while (node) {
+      if (counter === index) {
+        return node;
+      }
+      node = node.next;
+      counter++;
+    }
+
+    return null;
+  }
+}
 
 module.exports = { Node, LinkedList };
